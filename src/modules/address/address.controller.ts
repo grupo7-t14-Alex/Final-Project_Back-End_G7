ff96e57997a5799400b62860ae0bcdbd3639c4e0
bielssinho <bielnmjr@gmail.com>
2023-06-24 16:02:31 -0300

    fix: updateUserDto and retired console.logs

--- src/modules/address/address.controller.ts
+++ src/modules/address/address.controller.ts
@@ -1,4 +1,4 @@
-import { Controller,Body, Patch, Param, UseGuards, Request } from "@nestjs/common";
+import { Controller, Body, Patch, Param, UseGuards, Request } from "@nestjs/common";
 import { AddressService } from "./address.service";
 
 import { UpdateAddressDto } from "./dto/update-address.dto";
@@ -6,15 +6,15 @@ import { JWTAuthGuard } from "../auth/jwt.auth.guard";
 
 @Controller("address")
 export class AddressController {
-  constructor(private readonly addressService: AddressService) {}
-  
+  constructor(private readonly addressService: AddressService) { }
+
   @Patch(":id")
   @UseGuards(JWTAuthGuard)
   update(@Param("id") addressID: string, @Body() updateAddressDto: UpdateAddressDto, @Request() req) {
     const userId = req.user.id
-    console.log(userId)
-    return this.addressService.update(addressID, updateAddressDto,userId);
+
+    return this.addressService.update(addressID, updateAddressDto, userId);
   }
 
-  
+
 }
